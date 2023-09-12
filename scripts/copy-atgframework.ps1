4d6f1a228fc61c8a25a25af10cf86153a8781e1e
Clément Dreptin <cdreptin@gmail.com>
2023-09-13 00:52:13 +0200

    build: improved the copy-atgframework.ps1 script to only copy the necessary files

--- scripts/copy-atgframework.ps1
+++ scripts/copy-atgframework.ps1
@@ -1,5 +1,5 @@
-$AtgFrameworkSource = "$($env:XEDK)\Source\Samples\Common\*"
-$AtgFrameworkDestination = "$($PSScriptRoot)\..\deps\AtgFramework"
+$SourceDir = "$($env:XEDK)\Source\Samples\Common"
+$DestinationDir = "$($PSScriptRoot)\..\deps\AtgFramework"
 
 # Make sure that the Xbox 360 SDK is installed
 if (!$env:XEDK) {
@@ -7,10 +7,18 @@ if (!$env:XEDK) {
     Exit 1
 }
 
-# Copy the AtgFramework source code locally
+# Get the full path of each file included in vcxproj file
+$FilePaths = `
+    Select-Xml `
+    -Path "$($DestinationDir)\AtgFramework.vcxproj" `
+    -Namespace @{msbuild = "http://schemas.microsoft.com/developer/msbuild/2003"} `
+    -XPath "//msbuild:ClInclude/@Include | //msbuild:ClCompile/@Include" `
+    | foreach { "$($SourceDir)\$($_.Node."#text")"}
+
+# Copy the files
 try {
-    Copy-Item -Path $AtgFrameworkSource -Destination $AtgFrameworkDestination -Exclude "*.vcxproj*"
+    Copy-Item -Path $FilePaths -Destination $DestinationDir
 } catch {
-    echo "Could not copy $($AtgFrameworkSource) into $($AtgFrameworkDestination)!"
+    echo "Could not copy $($SourceDir) into $($DestinationDir)!"
     Exit 1
 }
